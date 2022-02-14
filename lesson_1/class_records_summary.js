325c63c598880efd341a0c29acd8e4a37520e76e
Chelsea Saunders <c_chilcoat@msn.com>
2022-02-13 23:50:58 -0800

    some refactoring

--- lesson_1/class_records_summary.js
+++ lesson_1/class_records_summary.js
@@ -1,140 +1,8 @@
 "use strict"
 
-// At the end of each term, faculty members need to prepare a class record
-// summary for students based on the weighted scores of exams and exercises. In
-// this practice problem, we will prepare one such summary from some provided
-// student records.
-
-// Exams and Exercises
-
-// Grading areas include exams and exercises, with the following weights:
-// Grading Area 	Weight
-// Exam 	65%
-// Exercises 	35%
-
-// Each term has four exams, and several exercises. Every exam has a fixed
-// maximum score of 100, while exercises have varied maximum score values and
-// counts. The total maximum point value for all exercises in any term is always
-// 100, regardless of how many exercises the students had to complete. For
-// example, a term may have five exercises with possible score maximums of
-// [30, 20, 10, 20, 20] while another term may have three exercises with
-// possible score maximums of [20, 30, 50].
-
-// To determine a student's grade, we first determine the student's average
-// score from the four exams, then sum all the exercise scores. We then apply
-// the weights to compute the student's final percent grade. Finally, we
-// determine the letter equivalent grade from the student's percent grade we
-// just computed.
-
-// Percent Grade 	Letter Equivalent
-// 93 - 100 	    A
-// 85 - 92      	B
-// 77 - 84      	C
-// 69 - 76 	      D
-// 60 - 68 	      E
-// 0 - 59 	      F
-
-// For example, let's assume a term with three exercises with maximum scores of
-// [20, 30, 50]. A student got [90, 80, 95, 71] on her four exams, and
-// [20, 15, 40] on her exercises. To determine her final grade, we follow these
-// steps:
-
-//     Compute the student's average exam score: (90 + 80 + 95 + 71) / 4 = 84
-//     Compute the student's total exercise score: 20 + 15 + 40 = 75
-//     Apply weights to determine the final percent grade:
-//                                                  84 * .65 + 75 * .35 = 80.85
-//     Round the percent grade to the nearest integer: 81
-//     Lookup the letter grade in the table above: C
-//     Combine the percent grade and letter grade: "81 (C)"
-
-// Source Data and Class Record Summary Format
-
-// We store the student data in an object that looks like this:
-
-// let studentScores = {
-//   student1: {
-//     id: <idNumber>,
-//     scores: {
-//       exams: [],
-//       exercises: [],
-//     },
-//   },
-//   student2: {
-//     id: <idNumber>,
-//     scores: {
-//       exams: [],
-//       exercises: [],
-//     },
-//   },
-//   student3: {
-//     id: <idNumber>,
-//     scores: {
-//       exams: [],
-//       exercises: [],
-//     },
-//   },
-//   studentN: {
-//     id: <idNumber>,
-//     scores: {
-//       exams: [],
-//       exercises: [],
-//     },
-//   },
-// };
-
-// The output class record summary should look like this:
-
-// {
-//   studentGrades: [ '87 (B)', '73 (D)', '84 (C)', '86 (B)', '56 (F)' ],
-//   exams: [
-//     { average: 75.6, minimum: 50, maximum: 100 },
-//     { average: 86.4, minimum: 70, maximum: 100 },
-//     { average: 87.6, minimum: 60, maximum: 100 },
-//     { average: 91.8, minimum: 80, maximum: 100 },
-//   ],
-// }
-
-// Round the exam averages to one digit after the decimal point.
-// Function Requirements
-
-// Given this information, implement a function that takes a studentScores
-// object and returns a class record summary object.
-
-// You can use the following code to test your implementation:
-
-/*
-DATA:
-Input: object of objects
-Output: object with grades property which is array of strings, and exams
-property which is array of objects
-
-given algo:
-//     Compute the student's average exam score: (90 + 80 + 95 + 71) / 4 = 84
-//     Compute the student's total exercise score: 20 + 15 + 40 = 75
-//     Apply weights to determine the final percent grade:
-//                                                  84 * .65 + 75 * .35 = 80.85
-//     Round the percent grade to the nearest integer: 81
-//     Lookup the letter grade in the table above: C
-//     Combine the percent grade and letter grade: "81 (C)"
-
-
-ALGO:
-create result object with two properies both assigned empty arrays:
-  studentGrades and exams
-generate array of keys for input object
-iterate through keys array, with each property as needed
-- compute grades
-    use accumulator to add exam score, then divide by that array length
-    use accumulator to add exercises score
-    apply weights --exercises *
-  - get averages
-
-  - convert to number grades
--
-*/
-
 const WEIGHTED_EXAM_VALUE = .65;
 const WEIGHTED_EXERCISES_VALUE = .35;
+const NUMBER_OF_EXAMS = 4;
 
 let studentScores = {
   student1: {
@@ -175,42 +43,44 @@ let studentScores = {
 };
 
 function generateClassRecordSummary(scores) {
-  let result = { studentGrades: [], };
-  let keysArr = Object.keys(scores);
-  let allExamsArr = [[], [], [], []];
+  let result = { studentGrades: [], exams: [[], [], [], []], };
+  let students = Object.keys(scores);
 
-  keysArr.forEach((student) => {
-    let examsArr = scores[student].scores.exams;
-    let exercisesArr = scores[student].scores.exercises;
+  compileGrades(students, scores, result.studentGrades);
+  compileExamStats(students, scores, result.exams);
+
+  return result;
+}
+
+function compileGrades(students, scoreList, resultArr) {
+  students.forEach((student) => {
+    let examsArr = scoreList[student].scores.exams;
+    let exercisesArr = scoreList[student].scores.exercises;
 
     let avgExam = computeExamAvg(examsArr);
     let avgExercise = computeExerciseAvg(exercisesArr);
     let avgGrade = computeAvgGrade(avgExam, avgExercise);
 
-    result.studentGrades.push(avgGrade);
-    organizeScoresByExam(examsArr, allExamsArr);
+    resultArr.push(avgGrade);
   });
+}
 
-  result.exams = allExamsArr.map((scores) => computeMinAvgMax(scores));
-
-  return result;
+function computeExamAvg(examScoreArr) {
+  let totalScore = examScoreArr.reduce((sum, currentVal) => sum + currentVal);
+  return totalScore / NUMBER_OF_EXAMS;
 }
 
 function computeExerciseAvg(exerciseScoreArr) {
   return exerciseScoreArr.reduce((total, currentVal) => total + currentVal);
 }
 
-function computeExamAvg(examScoreArr) {
-  let totalScore = examScoreArr.reduce((sum, currentVal) => sum + currentVal);
-  let numberOfExams = examScoreArr.length;
-  return totalScore / numberOfExams;
-}
-
 function computeAvgGrade(examAvg, exerciseAvg) {
   let weightedExamAvg = examAvg * WEIGHTED_EXAM_VALUE;
   let weightedExerciseAvg = exerciseAvg * WEIGHTED_EXERCISES_VALUE;
+
   let avgScore = Math.round(weightedExamAvg + weightedExerciseAvg);
   let letterGrade = determineLetterGrade(avgScore);
+
   return `${avgScore} (${letterGrade})`;
 }
 
@@ -230,6 +100,15 @@ function determineLetterGrade(avgScore) {
   }
 }
 
+function compileExamStats(studentList, scoreList, resultArr) {
+  studentList.forEach((student) => {
+    let examsArr = scoreList[student].scores.exams;
+    organizeScoresByExam(examsArr, resultArr);
+  })
+
+ resultArr = resultArr.map((scores) => computeMinAvgMax(scores));
+}
+
 function organizeScoresByExam(scoresArr, examArr) {
   scoresArr.forEach((score, index) => {
     examArr[index].push(score);
@@ -242,6 +121,7 @@ function computeMinAvgMax(examArr) {
   let min = examArr[0]
   let max = examArr[examArr.length - 1];
   let avg = computeExamAvg(examArr);
+
   return {average: avg, minimum: min, maximum: max};
 }
 
