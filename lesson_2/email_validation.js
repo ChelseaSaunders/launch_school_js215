a1bc3082c5610ddaa7e3e965a72f3df61c67324e
Chelsea Saunders <c_chilcoat@msn.com>
2022-02-15 14:11:31 -0800

    email validation exercise

--- lesson_2/email_validation.js
+++ lesson_2/email_validation.js
@@ -25,18 +25,72 @@
 // programs. We are using greatly simplified criteria to let you concentrate on
 // the fundamentals of JavaScript, and not on the specifics of email addresses.
 
+// PROBLEM:
+// RULES:
+//   EXPLICIT:
+//   - must have two parts separated by exactly one '@'
+//   - portion before @ must have only alphanumeric chars
+//   - portion after @ must have at least one '.' but cannot start/ end with '.'
+//   - apart from '.', all chars must be letters
+
+// DATA:
+//   INPUT: sting
+//   OUTPUT: boolean
+
+// ALGO:
+// - if string starts with or ends with @ return false
+// - split string on '@'
+//   - if that array length !== 2, return false
+// assign split arr[0] to local and split arr[1] to domain
+// helper function:
+// - check local; if any char is not a letter or number, return false
+// main: if check local is false, return false
+// helper function:
+// - check domain: if
+// main: if check domain is false, return false
+// return true
+
 function isValidEmail(email) {
-  // ...
+  let parts = email.split('@');
+  if (!validateParts(email, parts)) return false;
+
+  let local = parts[0];
+  if (!validateLocal(local)) return false;
+
+  let domain = parts[1];
+  if (!validateDomain(domain)) return false;
+
+  return true;
 }
 
-isValidEmail('Foo@baz.com.ph');          // returns true
-isValidEmail('Foo@mx.baz.com.ph');       // returns true
-isValidEmail('foo@baz.com');             // returns true
-isValidEmail('foo@baz.ph');              // returns true
-isValidEmail('HELLO123@baz');            // returns false
-isValidEmail('foo.bar@baz.to');          // returns false
-isValidEmail('foo@baz.');                // returns false
-isValidEmail('foo_bat@baz');             // returns false
-isValidEmail('foo@bar.a12');             // returns false
-isValidEmail('foo_bar@baz.com');         // returns false
-isValidEmail('foo@bar.....com');         // returns false
+function validateParts(email, parts) {
+  return (email.startsWith('@') || email.endsWith('@') || parts.length === 2);
+}
+
+function validateLocal(local) {
+  return !local.match(/[^a-z0-9]/ig)
+}
+
+function validateDomain(domain) {
+  if (domain.startsWith('.')
+      || domain.endsWith('.')
+      || !domain.includes('.')
+      || domain.match(/\.\./g)
+      || domain.match(/[^a-z.]/ig)) {
+     return false;
+  }
+
+  return true;
+}
+
+console.log(isValidEmail('Foo@baz.com.ph'));          // returns true
+console.log(isValidEmail('Foo@mx.baz.com.ph'));       // returns true
+console.log(isValidEmail('foo@baz.com'));             // returns true
+console.log(isValidEmail('foo@baz.ph'));              // returns true
+console.log(isValidEmail('HELLO123@baz'));            // returns false
+console.log(isValidEmail('foo.bar@baz.to'));          // returns false
+console.log(isValidEmail('foo@baz.'));                // returns false
+console.log(isValidEmail('foo_bat@baz'));             // returns false
+console.log(isValidEmail('foo@bar.a12'));             // returns false
+console.log(isValidEmail('foo_bar@baz.com'));         // returns false
+console.log(isValidEmail('foo@bar.....com'));         // returns false
